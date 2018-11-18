11108adb3528dc5793a046c63ed08953797f37b8
Isaac Leanos <isaac.leanos11@gmail.com>
2018-11-17 21:30:30 -0800

    move build to servers public dir & deploy to heroku

---
+++ public/precache-manifest.35606dd01afb7b504a0991aa54106b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/spaceX_logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf751d8db635c8218af3",
+    "url": "/static/js/main.bf751d8d.chunk.js"
+  },
+  {
+    "revision": "c8cdcb643becc4894683",
+    "url": "/static/js/1.c8cdcb64.chunk.js"
+  },
+  {
+    "revision": "bf751d8db635c8218af3",
+    "url": "/static/css/main.ac299bf0.chunk.css"
+  },
+  {
+    "revision": "9d78edb4a8909662f4870ffc1bd9dbaa",
+    "url": "/index.html"
+  }
+];
